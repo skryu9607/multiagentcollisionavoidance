7bbb94397d9f6bc437f08128ad8096c107c33529
MATLAB7 <crdhsh@gmail.com>
2023-07-28 10:47:47 +0900

    - refactoring applied to the entire project - simple readme.md added

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from VoronoiCA import VoronoiCA
+import numpy as np
+
+def main():
+
+    # make up data points
+    np.random.seed(10)
+    
+    # run process
+    voronoi_ca = VoronoiCA()
+    voronoi_ca.proc()
+    
+    print(1)
+
+if __name__ == "__main__":
+	main()

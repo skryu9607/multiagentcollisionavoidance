56711a16565a808e8765bcee0629c63ecd9095ec
Seung <skryu9607@kaist.ac.kr>
2023-08-22 04:26:59 +0900

    Aug 22th

--- src/cbba/util.py
+++ src/cbba/util.py
@@ -3,6 +3,7 @@ import numpy as np
 class Util():
     def __init__(self) -> None:
         pass
-
+    # 이제 이 부분이 prm으로 바뀔 예정
     def calc_dist(self, pos_0, pos_1):
         return np.linalg.norm(pos_1 - pos_0)
+정정
